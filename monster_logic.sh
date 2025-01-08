88c41dc0edc9cd8f188a62aa31a97ac0e95d310d
kay-hk <kay.kazmierczak@outlook.com>
2025-01-08 11:06:04 +0000

    monster logic with playerhealth

--- monster_logic.sh
+++ monster_logic.sh
@@ -1,13 +1,28 @@
 #! /bin/bash
 
-oppohealth=$((RANDOM % 30))
+playerhealth=30
+oppohealth=$((RANDOM % 28 + 3))
+oppodam=$((RANDOM % 10))
+monster_type=""
 
-if [ $oppohealth -lt 10 ]; then
+if [ $oppohealth -le 10 ]; then
+    monster_type="Goblin"
     echo "It's a Goblin!"
-elif [ $oppohealth -ge 10 ] && [ $oppohealth -lt 20 ]; then
+    echo "Monster health: $oppohealth"
+elif [ $oppohealth -gt 10 ] && [ $oppohealth -lt 20 ]; then
+    monster_type="Kobold"
     echo "It's a Kobold!"
+    echo "Monster health: $oppohealth"
 elif [ $oppohealth -ge 20 ] && [ $oppohealth -lt 30 ]; then
+    monster_type="Orc"
     echo "It's an Orc!"
+    echo "Monster health: $oppohealth"
 fi
 
-echo $oppohealth
+if [ "$monster_type" == "Goblin" ]; then
+    playerhealth=$((playerhealth - oppodam))
+elif [ "$monster_type" == "Kobold" ]; then
+    playerhealth=$((playerhealth - oppodam - 5))
+elif [ "$monster_type" == "Orc" ]; then
+    playerhealth=$((playerhealth - oppodam - 10))
+fi
