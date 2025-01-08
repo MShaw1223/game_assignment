a58abe1b4eae124961f936d5260baaf05b695e80
Miller <millershaw61@gmail.com>
2025-01-08 18:49:41 +0000

    Feat: Pickup loot and case insensitive inputs
    Using [xX] in the case statements input can be lower or upper, preventing a needless error. Added the array moves to track the moves the player makes, a stack would be preferable for current use as only the last value is needed for the revert moves and repeat moves functions.

--- game.sh
+++ game.sh
@@ -12,7 +12,6 @@ map=(
   0 0 0 0 0 0 0 0 0 0 0
 )
 
-# Helper function to access the map
 get_map_value() {
   local row=$1
   local col=$2
@@ -20,58 +19,178 @@ get_map_value() {
   echo "${map[$index]}"
 }
 
-curr_row=5
-curr_col=5
+moves=("None")
 
-tiles=("wall" "path" "loot" "enemy")
+score=0
+curr_row=6
+curr_col=7
+
+tiles=("wall" "path" "loot" "door" "enemy")
+
+revertLastMove(){
+  local last=$1
+
+  case $last in 
+    [aA])
+      moves+=($last)
+      ((curr_col++));;
+    [wW])
+      moves+=($last)
+      ((curr_row++));;
+    [sS])
+      moves+=($last)
+      ((curr_row--));;
+    [dD])
+      moves+=($last)
+      ((curr_col--));;
+    *)
+      echo "Error reading last move";;
+  esac
+}
+
+repeatLastMove(){
+  local last=$1
+
+  case $last in 
+    [aA])
+      ((curr_col--));;
+    [wW])
+      ((curr_row--));;
+    [sS])
+      ((curr_row++));;
+    [dD])
+      ((curr_col++));;
+    *)
+      echo "Error reading last move";;
+  esac
+}
 
 while true; do
+  len_moves=${#moves[@]}
+  last_move_index=$((len_moves-1))
+  last_move=${moves[$last_move_index]}
+
+  echo "last move: $last_move"
   echo "Current Position: ($curr_row, $curr_col)"
   current_value=$(get_map_value "$curr_row" "$curr_col")
-  echo "You are standing on: ${tiles[$current_value]}"
 
-  read -p "I: inventory | M: move | C: Check score | Q: Quit " choice
+  if ((current_value == 2));then
+    echo "You are standing on: ${tiles[$current_value]}"
+    read -p "P to pickup loot, b to go back: " pickup
+      case $pickup in
+        [pP])
+          echo "Loot picked up, +1 score!"
+          ((score++))
+          repeatLastMove $last_move;;
+        [bB])
+          revertLastMove $last_move;;
+        *)
+          echo "Input incorrect, loot not picked up.";;
+    esac
+    else
+      echo "You are standing on: ${tiles[$current_value]}"
+  fi
+
+  echo "I: inventory | M: move | C: Check score | Q: Quit "
+  read -p "> " choice
   case $choice in
-    i) 
+    [iI]) 
       echo "Inventory";;
-    c) 
-      echo "Check score";;
-    m) 
+    [cC]) 
+      echo "Current score: $score";;
+    [mM]) 
       read -p "Enter A/W/S/D: " movement
       case $movement in
-        a)
+        [aA])
           next_tile=$(get_map_value "$curr_row" "((curr_col-1))")
           if ((curr_col > 0 && next_tile > 0)); then
-            ((curr_col--))
+            if ((next_tile == 1 || next_tile == 2));then
+              ((curr_col--))
+              moves+=($movement)
+            elif ((next_tile==3));then
+              read -p "A to continue through door, B to go back: " choice
+              case $choice in
+                [aA])
+                  ((curr_col--))
+                  moves+=($choice);;
+                [bB])
+                  echo "Turning around...";;
+                *)
+                  echo "Incorrect input";;
+              esac
+            fi
           else
             echo "Can't move left!"; 
           fi;;
-        w)
+        [wW])
           next_tile=$(get_map_value "((curr_row-1))" "$curr_col")
-          if ((curr_row > 0 && next_tile > 0));then
-            ((curr_row--))
+          if ((curr_col > 0 && next_tile > 0)); then
+            if ((next_tile == 1 || next_tile == 2));then
+              ((curr_row--))
+              moves+=($movement)
+            elif ((next_tile==3));then
+              read -p "W to continue through door, B to go back: " choice
+              case $choice in
+                [wW])
+                  ((curr_row--))
+                  moves+=($choice);;
+                [bB])
+                  echo "Turning around...";;
+                *)
+                  echo "Incorrect input";;
+              esac
+            fi
           else
             echo "Can't move up!"
           fi;;
-        s)
+        [sS])
           next_tile=$(get_map_value "((curr_row+1))" "$curr_col")
-          if ((curr_row > 0 && next_tile > 0)); then
-            ((curr_row++))
+          if ((curr_col > 0 && next_tile > 0)); then
+            if ((next_tile == 1 || next_tile == 2));then
+              ((curr_row++))
+              moves+=($movement)
+            elif ((next_tile==3));then
+              read -p "S to continue through door, B to go back: " choice
+              case $choice in
+                [sS])
+                  ((curr_row++))
+                  moves+=($choice);;
+                [bB])
+                  echo "Turning around...";;
+                *)
+                  echo "Incorrect input";;
+              esac
+            fi
           else
             echo "Can't move down!"
           fi;;
-        d)
+        [dD])
           next_tile=$(get_map_value "$curr_row" "$((curr_col+1))")
           if ((curr_col > 0 && next_tile > 0)); then
-            ((curr_col++))
+            if ((next_tile == 1 || next_tile == 2));then
+              ((curr_col++))
+              moves+=($movement)
+            elif ((next_tile==3));then
+              read -p "D to continue through door, b to go back: " choice
+              case $choice in
+                [dD])
+                  ((curr_col++))
+                  moves+=($choice);;
+                [bB])
+                  echo "Turning around...";;
+                *)
+                  echo "Incorrect input";;
+              esac
+            fi
           else
             echo "Can't move right!"; 
           fi;;
         *)
           echo "incorrect";;
         esac;;
-    q) 
-      echo "Quiting game..."; 
+    [qQ]) 
+      echo "Final score: $score"
+      echo "Quiting game..."
       break;;
     *) 
       echo "Incorrect input";;
