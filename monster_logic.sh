63b9d964a77511b2decfa0f5c8a122e60dfdce79
kay-hk <kay.kazmierczak@outlook.com>
2025-01-08 21:59:48 +0000

    fix: monster type, monster attack; add: flee logic

--- monster_logic.sh
+++ monster_logic.sh
@@ -1,55 +1,62 @@
 #! /bin/bash
 
-player_health=30
+# Global variables
+initial_player_health=30
+player_health=$initial_player_health
 oppo_type=""
 oppo_health=0
 
+# Random monster types
 monster_type(){
     oppo_health=$((RANDOM % 28 + 3))
     if [ $oppo_health -le 10 ]; then
-        oppo_type="GOBLIN"
-        echo "It's a GOBLIN!"
+        oppo_type="GHOUL"
+        echo "A wild $oppo_type appears!"
         echo "$oppo_type's health: $oppo_health"
     elif [ $oppo_health -gt 10 ] && [ $oppo_health -lt 20 ]; then
-        oppo_type="KOBOLD"
-        echo "It's a KOBOLD!"
+        oppo_type="VAMPIRE"
+        echo "A wild $oppo_type appears!"
         echo "$oppo_type's health: $oppo_health"
     elif [ $oppo_health -ge 20 ] && [ $oppo_health -lt 30 ]; then
-        oppo_type="OGRE"
-        echo "It's an OGRE!"
+        oppo_type="WEREWOLF"
+        echo "A wild $oppo_type appears!"
         echo "$oppo_type's health: $oppo_health"
     fi
 }
 
+# Player attacks and damage 
 player_attack(){
-    player_dam=0
-    attack=$((RANDOM % 30))
+    player_dmg=0
+    attack=$((RANDOM % 31))
     if [ $attack -le 10 ]; then
-        player_dam=$((RANDOM % 8 + 3))
-        oppo_health=$((oppo_health - player_dam))
-        echo "You SLASH the $oppo_type for $player_dam damage!"
+        player_dmg=$((RANDOM % 8 + 3))
+        oppo_health=$((oppo_health - player_dmg))
+        echo "You SLASH the $oppo_type for $player_dmg damage!"
     elif [ $attack -gt 10 ] && [ $attack -lt 20 ]; then
-        player_dam=$((RANDOM % 13 + 3))
-        oppo_health=$((oppo_health - player_dam))
-        echo "You CHOP the $oppo_type for $player_dam damage!"
+        player_dmg=$((RANDOM % 13 + 3))
+        oppo_health=$((oppo_health - player_dmg))
+        echo "You CHOP the $oppo_type for $player_dmg damage!"
     elif [ $attack -ge 20 ] && [ $attack -lt 30 ]; then
-        player_dam=$((RANDOM % 18 + 3))
-        oppo_health=$((oppo_health - player_dam))
-        echo "You STAB the $oppo_type for $player_dam damage!"
+        player_dmg=$((RANDOM % 18 + 3))
+        oppo_health=$((oppo_health - player_dmg))
+        echo "You STAB the $oppo_type for $player_dmg damage!"
     fi
 }
 
+# Monster attacks and damage
 monster_attack(){
-    oppo_dam=$((RANDOM % 10))
-    if [ "$oppo_type" == "GOBLIN" ]; then
-        player_health=$((player_health - oppo_dam))
-        echo "The $oppo_type attacks you for $oppo_dam damage!"
-    elif [ "$oppo_type" == "KOBOLD" ]; then
-        player_health=$((player_health - oppo_dam - 5))
-        echo "The $oppo_type attacks you for $oppo_dam damage!"
-    elif [ "$oppo_type" == "OGRE" ]; then
-        player_health=$((player_health - oppo_dam - 10))
-        echo "The $oppo_type attacks you for $oppo_dam damage!"
+    oppo_dmg=$((RANDOM % 11))
+    if [ "$oppo_type" == "GHOUL" ]; then
+        player_health=$((player_health - oppo_dmg))
+        echo "The $oppo_type attacks you for $oppo_dmg damage!"
+    elif [ "$oppo_type" == "VAMPIRE" ]; then
+        vampire_dmg=$(( oppo_dmg + 5 ))
+        player_health=$((player_health - vampire_dmg))
+        echo "The $oppo_type attacks you for $vampire_dmg damage!"
+    elif [ "$oppo_type" == "WEREWOLF" ]; then
+        werewolf_dmg=$(( oppo_dmg + 10 ))
+        player_health=$((player_health - werewolf_dmg))
+        echo "The $oppo_type attacks you for $werewolf_dmg damage!"
     fi
 }
 
@@ -59,20 +66,22 @@ player_health_check(){
     fi
 }
 
+# Health potion 
 use_health_potion(){
     health_potion=$((RANDOM % 9 + 2))
-    if (( player_health < 30 )); then
+    if (( player_health < initial_player_health )); then
         player_health=$(( player_health + health_potion ))
         echo "You restored $health_potion health!"
     else
         echo "You are already at maximum health!"
     fi
-    if (( player_health > 30)); then 
-        player_health=30
+    if (( player_health > initial_player_health)); then 
+        player_health=$initial_player_health
     fi
     echo "Player health: $player_health"
 }
 
+# Main fight logic
 fight_loop(){
     monster_type  
     while [ $player_health -gt 0 ] && [ $oppo_health -gt 0 ]; do  
@@ -85,8 +94,16 @@ fight_loop(){
                 break
             fi
         elif (( ch == 2 )); then
-            echo "You flee the battle."
-            break
+            echo "You try to flee the battle."
+            flee=$(( RANDOM % 11 ))
+            if (( flee <= 5 )); then 
+                monster_attack
+                echo "Wounded, you manage to escape."
+                break
+            elif (( flee > 5 )); then
+                echo "You manage to escape unscratched!"
+                break
+            fi
         elif (( ch == 3 )); then
             use_health_potion
             continue   
